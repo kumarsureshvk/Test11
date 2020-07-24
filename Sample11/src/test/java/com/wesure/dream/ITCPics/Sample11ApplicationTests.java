1b9ae0c6986d499cafb81d4cf3c5935936267523
welcome.suresh2012@gmail.com <welcome.suresh2012@gmail.com>
2020-07-24 08:15:49 +0530

    first commit

---
+++ Sample11/src/test/java/com/wesure/dream/ITCPics/Sample11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesure.dream.ITCPics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sample11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
